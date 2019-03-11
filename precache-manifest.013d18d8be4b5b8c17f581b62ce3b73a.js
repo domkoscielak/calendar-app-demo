c8a9e4a395327970b536ccdeff91981db90ca92a
Dominik Koscielak <domkoscielak@gmail.com>
2019-03-11 14:06:48 +0100

    Updates

---
+++ precache-manifest.013d18d8be4b5b8c17f581b62ce3b73a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f2ad9512c7ad4ea794d3a5d6adbd69e",
+    "url": "/calendar-app-demo/static/media/spiner.2f2ad951.gif"
+  },
+  {
+    "revision": "a84cd9c053dc6b8e854e",
+    "url": "/calendar-app-demo/static/js/runtime~main.a84cd9c0.js"
+  },
+  {
+    "revision": "eaf9a0773a1750424a44",
+    "url": "/calendar-app-demo/static/js/main.eaf9a077.chunk.js"
+  },
+  {
+    "revision": "94beb0a33a60fc9e3ec7",
+    "url": "/calendar-app-demo/static/js/2.94beb0a3.chunk.js"
+  },
+  {
+    "revision": "eaf9a0773a1750424a44",
+    "url": "/calendar-app-demo/static/css/main.062a9b9a.chunk.css"
+  },
+  {
+    "revision": "a95a0a6155d9ad2c0a92dc152fece213",
+    "url": "/calendar-app-demo/index.html"
+  }
+];
